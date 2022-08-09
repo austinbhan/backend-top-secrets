72a1c96104ff29f17b346488b0252c194e149dbc
austinbhan <austinbhan@outlook.com>
2022-08-08 17:22:32 -0700

    SQL tables created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,28 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS secrets;
+
+CREATE TABLE users (
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE secrets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR NOT NULL
+    description VARCHAR NOT NULL
+    created_at TIMESTAMP
+)
+
+INSERT INTO secrets (
+    title,
+    description,
+    created_at
+)
+VALUES
+('Sacramento','CA')
+('Salem','OR')
+('Olympia','WA')
+('Carson City','NV')
